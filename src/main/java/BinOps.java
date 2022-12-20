ebf30ef9a85737511a2a5ea8fa867e360abcd232
Victor Alyonkin <viktoalenkin@ya.ru>
2022-12-20 15:25:55 +0300

    first commit

---
+++ src/main/java/BinOps.java
@@ -0,0 +1,21 @@
+public class BinOps {
+    public String sum(String a, String b) {
+    /*Переведите числа из двоичной записи в
+      строках в int, сложите и переведите
+      обратно в двоичную запись*/
+        int arg0 = Integer.parseInt(a, 2);
+        int arg1 = Integer.parseInt(b, 2);
+        int result = arg0 + arg1;
+        return Integer.toBinaryString(result);
+    }
+
+    public String mult(String a, String b) {
+    /*Переведите числа из двоичной записи в
+      строках в int, перемножьте и переведите
+      обратно в двоичную запись*/
+        int arg0 = Integer.parseInt(a, 2);
+        int arg1 = Integer.parseInt(b, 2);
+        int result = arg0 * arg1;
+        return Integer.toBinaryString(result);
+    }
+}
